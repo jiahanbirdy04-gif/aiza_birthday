c21d16d9b0d8c1f83b76e73219afd2b388a2cc0d
jiahanbirdy04-gif <jiahanbirdy04@gmail.com>
2026-09-18 04:03:39 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "bday-maker-v1";
+const CACHE_NAME = "bday-maker-v2";
 const CORE_ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
